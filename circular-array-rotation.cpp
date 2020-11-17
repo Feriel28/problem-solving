82df8ec8afb240f441fc88a174945f9b7dcdb04b
fifi <fbabay143@gmail.com>
2020-11-17 21:13:38 +0100

    adding solution

---
+++ circular-array-rotation.cpp
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: fifi
+ *
+ * Created on November 14, 2020, 3:39 PM
+ */
+
+#include <bits/stdc++.h>
+#include<string>
+#include <iostream>
+
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the circularArrayRotation function below.
+
+vector<int> circularArrayRotation(vector<int> a, int k, vector<int> queries) {
+    int l = a.size();
+    vector<int> b(l, 0);
+    //b[k] = a[0];
+    for (int i = 0; i < l; i++) {
+        int left =  (k+i)%l ;
+        //int right = 1 + l % i;
+        //cout << left << endl;
+        b[left] = a[i];
+
+    }
+    int lq = queries.size();
+    vector<int> bq(lq, 0);
+    for (int i = 0; i < lq; i++) {
+        bq[i] = b[queries[i]];
+
+    }
+    return (bq);
+}
+
+int main(int argc, char** argv) {
+    int k;
+    cout << "nb de rotation"<< endl;
+    cin >> k;
+
+    vector<int> a(0);
+    int n;
+    cout << "taille du vecteur"<< endl;
+    cin >> n;
+    while (n--) {
+        int input;
+        cin >> input;
+        a.push_back(input);
+    }
+
+
+    vector<int> q(0);
+    int b;
+    cout << "nb of queries"<< endl;
+    cin >> b;
+    while (b--) {
+        int inputb;
+        cin >> inputb;
+        q.push_back(inputb);
+    }
+
+    cout << "output" << endl;
+    auto v = circularArrayRotation(a, k, q);
+    cout << "vector output" << endl;
+    for (int i = 0; i < v.size(); i++) {
+        cout << v[i];
+        cout << "\n";
+    }
+
+    return 0;
+}
+
+
